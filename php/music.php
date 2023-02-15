886c10daafb425ee65298995140cb82462fdebb0
Gohsty <kvdisukv2@gmail.com>
2023-02-15 18:28:12 +0100

    new

---
+++ php/music.php
@@ -0,0 +1,13 @@
+<?php
+$playAudio = "/mp3/fondo.mp3";
+
+function playAudio() {
+  var audio = document.getElementById("background-music");
+  audio.play();
+}
+
+//function setVolume() {
+//  var audio = document.getElementById("background-music");
+//  audio.volume = document.querySelector("input[type=range]").value;
+//}
+?>
